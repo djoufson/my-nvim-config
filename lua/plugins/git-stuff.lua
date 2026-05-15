099bb54f0be7fe8299414470a9dbed3aa0abe49c
Djoufson CHE BENE <djouflegran@gmail.com>
2026-05-15 23:24:11 +0100

    add automatic inline blame

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -5,7 +5,16 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        -- GitLens-style inline blame at the end of the current line.
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol",
+          delay = 300,
+        },
+        current_line_blame_formatter = "  <author>, <author_time:%R> · <summary>",
+      })
     end
   }
 }
