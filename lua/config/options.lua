54dcbaf341eccf55c19e2cb78cb2f65d1faefba5
Djoufson CHE BENE <djouflegran@gmail.com>
2026-05-16 16:05:39 +0100

    add default indentation and fat cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,16 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Indentation: 4 spaces, no hard tabs.
+vim.opt.expandtab = true -- insert spaces instead of a tab character
+vim.opt.shiftwidth = 4 -- width of an indent step (<<, >>, autoindent)
+vim.opt.tabstop = 4 -- a literal tab renders as 4 columns
+vim.opt.softtabstop = 4 -- <Tab>/<BS> in insert mode acts on 4 spaces
+vim.opt.scrolloff = 8 -- scroll offset
+
 vim.opt.autoread = true
 vim.opt.updatetime = 1000
 
 vim.o.autowriteall = true
+
+vim.opt.guicursor = "" -- fat cursor
